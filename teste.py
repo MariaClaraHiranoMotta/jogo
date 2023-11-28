f09297a42d4df6c3375091333594b08164a91da7
linafq <furukawaqueiroz@gmail.com>
2023-11-28 17:46:37 -0300

    classes funcionando

--- teste.py
+++ teste.py
@@ -11,6 +11,9 @@ pygame.mixer.init()  #  inicialização do módulo de áudio do pygame;
 
 window = pygame.display.set_mode((largura, altura)) # janela com 600 pixels de largura e 300 pixels de altura
 pygame.display.set_caption('Pac burguer') # titulo do jogo 
+font = pygame.font.SysFont(None, 48)
+background = pygame.image.load('fundo_tela.jpg').convert()   # tela de fundo 
+background_rect = background.get_rect()
 
 # Carrega os sons do jogo
 pygame.mixer.music.load('joker-boy-109889.mp3')
@@ -23,16 +26,18 @@ all_sprites = pygame.sprite.Group()
 player = jogador()
 all_sprites.add(player)
 
-clock = pygame.time.Clock()    # analisar troca para speed
+clock = pygame.time.Clock()    
 
 ref = pygame.time.get_ticks()
 game = True 
 
  
 pygame.mixer.music.play(loops=-1)  # começa a tocar o som de fundo em loop;
+
 while game :      # loop principal 
     clock.tick(FPS)   # Quanto mais frames, mais rápido o objeto vai se mexer 
     window.fill((0,0,0))       # preenche a tela com  a cor preta para não mostrar a trajetória do objeto
+
     for event in pygame.event.get() :
         if event.type == pygame.QUIT :
             game = False
@@ -53,29 +58,16 @@ while game :      # loop principal
 
     now = pygame.time.get_ticks()
 
-    if now - ref >= 1000:
+    if now - ref >= 1000:     # criar bombas e hamburguer novos a cada 1 segundo 
         objeto_obstaculo = obstaculo()
-        objeto_comida = comida()
+        objeto_comida = comida(objeto_obstaculo.rect.x)
         all_sprites.add(objeto_obstaculo, objeto_comida)
         ref = now
     
     all_sprites.update()
-    window.fill((0,0,0))
+    window.blit(background, background_rect)
     all_sprites.draw(window)
 
-    pygame.display.update()  # a cada iteração do loop principal do jogo a tela é atualizada 
 
-    # if state == GAME:'
-    # # Verifica se houve colisão entre player e bomba
-    #     hits = pygame.sprite.spritecollide(player, all_bomb, True)
-    #     if len(hits) > 0:
-    #         # Toca o som da colisão
-    #         explosion_sound.play()
-    #         player.kill()
-    #         explosao = Explosion(player.rect.center, assets)
-    #         all_sprites.add(explosao)
-    #         state = EXPLODING
-    #         keys_down = {}
-    #         explosion_tick = pygame.time.get_ticks()
-    #         explosion_duration = explosao.frame_ticks * len(explosao.explosion_anim) + 400
-            
+
+    pygame.display.update()  # a cada iteração do loop principal do jogo a tela é atualizada 
