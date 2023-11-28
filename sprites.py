325f530503dbac9021f90a0f9330d5b92a0b3e07
linafq <furukawaqueiroz@gmail.com>
2023-11-28 16:38:04 -0300

    velocidade do hamburguer trocada

--- sprites.py
+++ sprites.py
@@ -1,6 +1,6 @@
 import pygame
-from pygame.sprite import _Group 
-from janela import *
+from config import *
+import random
 
 class jogador(pygame.sprite.Sprite): 
     def __init__(self) :
@@ -10,7 +10,7 @@ class jogador(pygame.sprite.Sprite):
         self.image = pygame.image.load('pacman.png').convert_alpha()  # carrega imagem 
         self.image = pygame.transform.scale(self.image, (50, 50))     # tamamnho da imagem 
         self.rect = self.image.get_rect()      # monta perímetro para imagem 
-        self.rect.y = altura - 30
+        self.rect.bottom = altura
         self.rect.x = largura/2
         self.speedx = 0
     
@@ -18,15 +18,13 @@ class jogador(pygame.sprite.Sprite):
         self.rect.x += self.speedx
 
         if self.rect.x <= 0 :       # se o retangulo tentar passar da tela ele irá parar 
-            sel.speedx = 0
-
-        if serf.rect.x 
+            self.speedx = 0
 
         # Mantem dentro da tela
         if self.rect.right > largura:
-            self.rect.right = largura
+            self.speedx = 0
         if self.rect.left < 0:
-            self.rect.left = 0
+            self.speedx = 0
 
 
 
@@ -39,13 +37,12 @@ class obstaculo(pygame.sprite.Sprite):
         self.image = pygame.image.load('bomb.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (50, 50))
         self.rect = self.image.get_rect()
-        self.rect.y = 
-        self.rect.x = 0
-        self.speedy = 0
+        self.rect.y = -50
+        self.rect.x = random.randint(50,largura - 50)
+        self.speedy = 5
 
     def update(self):
          # Atualizando a posição da bomba
-        self.rect.x += self.speedx
         self.rect.y += self.speedy
         
 
@@ -57,15 +54,13 @@ class comida(pygame.sprite.Sprite):
 
         self.image = pygame.image.load('hamburger.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (50, 50))
-        self.,rect = self.image.get_rect()
-        self.rect.y = 
-        self.rect.x = 
-        self.speedx = 0
-        self.speedy = 0
+        self.rect = self.image.get_rect()
+        self.rect.y = -50
+        self.rect.x = random.randint(50,largura - 50)
+        self.speedy = 4
 
     def update(self): 
          # Atualizando a posição do hamburger
-        self.rect.x += self.speedx
         self.rect.y += self.speedy
         
        
